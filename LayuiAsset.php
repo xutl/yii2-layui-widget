97aac0d1be3ce04ba5da8e6e0f7c1e072526b91b
xuchao <85825770@qq.com>
2017-04-20 15:15:54 +0800

    init

---
+++ LayuiAsset.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace xutl\layui;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * Class LayuiAsset
+ * @package xutl\bootstrap\datetimepicker
+ */
+class LayuiAsset extends AssetBundle
+{
+    /**
+     * @inherit
+     */
+    public $sourcePath = '@vendor/xutl/yii2-layui-widget/assets';
+
+    /**
+     * @inherit
+     */
+    public $css = [
+        'css/layui.css',
+    ];
+
+    /**
+     * @inherit
+     */
+    public $js = [
+        'layui.js',
+    ];
+
+    /**
+     * @var array 定义依赖
+     */
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+}
